433d9c813038afd7c2bf610fa23bd27ba593cc4f
mrfosse <mrfosse99@gmail.com>
2016-10-12 18:09:32 -0700

    fixed the description using betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+  * main - prints a line
+  * Description: still prints a fancy line
+  * section header: not sure what this is
+  * Return: This describes void which is void
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
