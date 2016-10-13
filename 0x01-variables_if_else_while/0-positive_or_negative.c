495f28276d5e9fb53adbaf8aa423f65b9575c044
mrfosse <mrfosse99@gmail.com>
2016-10-13 12:38:03 -0700

    added 0-random

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - makes a random number
+ *Description: compares random number to zero to see if negative or positive
+ *Section header: test2
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is possitive\n", (n));
+	}
+		else if (n == 0)
+	{
+		printf("%d is zero\n", (n));
+	}
+	else
+	{
+		printf("%d is negative\n", (n));
+	}
+	return (0);
+}
