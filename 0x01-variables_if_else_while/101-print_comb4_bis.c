1662c685871a397de299a8c252b162e40c7aeae6
mrfosse <mrfosse99@gmail.com>
2016-10-14 18:59:34 -0700

    fixes maybe

---
+++ 0x01-variables_if_else_while/101-print_comb4_bis.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - print 000 to 999
+ *Description: prints all 000 to 999 individually
+ * with without duplicate numbers.
+ *header section: problem 11
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	int hundo;
+
+	hundo = 0;
+
+	while (hundo <= 7)
+
+	{
+		hundo++;
+	}
+	putchar('\n');
+	return (0);
+}
