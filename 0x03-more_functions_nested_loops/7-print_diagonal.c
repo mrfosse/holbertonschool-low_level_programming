ef6ba947319c17c6344cdedc1a2ba8c5f8a98a06
mrfosse <mrfosse99@gmail.com>
2016-10-21 13:16:41 -0700

    added 7

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * Description: same as above
+ * @n: number of paces to print, also number of lines to print.
+ * Return: returns none.
+ */
+void print_diagonal(int n)
+{
+	int a, b, c;
+
+	b = 0;
+	c = n;
+
+	if (n > 0)
+	{
+		while (b <= n)
+		{
+			a = c;
+			while (a <= n)
+			{
+				_putchar(' ');
+				a++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			b++;
+			c--;
+		}
+	}
+	 _putchar('\n');
+}
