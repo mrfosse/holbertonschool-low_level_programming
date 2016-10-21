7570b97df81e1cad6f131a44efdbea2f759629a1
mrfosse <mrfosse99@gmail.com>
2016-10-21 14:00:00 -0700

    swapeed buzz and fizz

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,9 @@ int main(void)
 		if ((a % 3 == 0) && (a % 5 == 0))
 			printf("FizzBuzz");
 		else if (a % 5 == 0)
-			printf("Fizz");
-		else if (a % 3 == 0)
 			printf("Buzz");
+		else if (a % 3 == 0)
+			printf("Fizz");
 		else
 			printf("%d", a);
 		if (!(a == 100))
