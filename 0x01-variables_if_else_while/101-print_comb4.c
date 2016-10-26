d77eb7e0dd8779fa671ba3645d8891e7542e57e3
mrfosse <mrfosse99@gmail.com>
2016-10-26 14:55:40 -0700

    merging

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,39 +10,33 @@
 
 int main(void)
 {
-	int hundo = 0;
-	int tens = 0;
-	int ones = 0;
-	int tenscount = 0;
-	int onescount = 0;
-	int countcount = 0;
+	int hundo;
+	int tens;
+	int ones;
 
-	while (hundo <= 7)
+	for (hundo = 0; hundo < 10 ; hundo++)
 	{
-		countcount++;
-		tenscount++;
-		onescount = countcount;
-		tens = tenscount;
-		ones = onescount;
-		while (tens <= 8)
+		for (tens = 0; tens < 10; tens++)
 		{
-			onescount++;
-			ones = onescount;
-			while (ones <= 9)
+			for (ones = 0; ones < 10; ones++)
 			{
 				putchar(hundo + '0');
 				putchar(tens + '0');
 				putchar(ones + '0');
-				if (!(hundo + tens + ones == 24))
+				if (hundo != tens && hundo < tens && hundo != ones && tens < ones)
 				{
+					putchar(hundo + '0');
+	                                putchar(tens + '0');
+        	                        putchar(ones + '0');
+					if (ones == 7)
+					{
+						break;
+					}
 					putchar(',');
 					putchar(' ');
 				}
-				ones++;
 			}
-			tens++;
 		}
-		hundo++;
 	}
 	putchar('\n');
 	return (0);
