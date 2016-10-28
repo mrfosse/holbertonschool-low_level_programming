c80165acc2a78d2e631ecbb1d2cbb4d10da509d0
mrfosse <mrfosse99@gmail.com>
2016-10-28 12:09:17 -0700

    added 5

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * string_toupper - copies an array in reverse.
- * Description: copies an array and reverses it.
- * @*: string to copy.
- * Return: returns the string dest.
+ * string_toupper - makes lower case letters uppercase.
+ * Description: copies an array and makes lower case to upper case.
+ * @p: pointer to copy
+ * Return: none.
  */
 char *string_toupper(char *p)
 {
@@ -13,7 +13,7 @@ char *string_toupper(char *p)
 
 	while (p[count] != '0')
 	{
-		if(p[count] >= 'a'&& p[count] <= 'z')
+		if (p[count] >= 'a' && p[count] <= 'z')
 		{
 			p[count] = p[count] - 32;
 		}
