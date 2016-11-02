79229c3c814424fb72869f058060290553dac4c8
mrfosse <mrfosse99@gmail.com>
2016-11-02 14:28:39 -0700

    added 2, plus some fixes

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -3,13 +3,14 @@
  * _strlen_recursion - prints a string in reverse.
  * Description: prints a string one char at a time backwards.
  * @s: string to be printed.
- * Return: returns none.
+ * Return: length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return(s + '0');
-		_strlen_recursion(s + 1);
+		return(1 + _strlen_recursion(s + 1));
 	}
+	return (0);
+
 }
