9a0b5151e6e18564c8f14f51d94094c2b5fafea3
mrfosse <mrfosse99@gmail.com>
2016-11-03 10:57:37 -0700

    redid 6 the correct way.

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,23 +1,31 @@
 #include "holberton.h"
 /**
- * is_prime_number - checks to see if a number is prime.
+ * _checker - checks to see if a number is prime.
+ * @n: the number to be checked.
+ * @check: the number to check n with.
+ * Return: returns 0 if prime, otherwise 1.
+ */
+int _checker(int n, int check)
+{
+	if (check <= 1)
+		return (1);
+	if (n % check == 0)
+		return (0);
+	return (_checker(n, check - 1));
+}
+/**
+ * is_prime_number - main func to see if num is prime
  * @n: the number to be checked.
  * Return: returns 1 if prime, otherwise 0.
  */
 int is_prime_number(int n)
 {
+	int check;
+
 	if (n <= 1)
 	{
 		return (0);
 	}
-	if (n % 10 == 0 || n % 9 == 0 || n % 8 == 0
-	|| n % 7  == 0 || n % 6 == 0 || n % 5 == 0 || n % 4 == 0
-	|| n % 4 == 0 || n % 3 == 0 || n % 2 == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+	check = n / 2;
+	return (_checker(n, check));
 }
