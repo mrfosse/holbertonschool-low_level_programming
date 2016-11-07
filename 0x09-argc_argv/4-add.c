a8d18e568fdb766bfbbcf518e82055fdbe281438
mrfosse <mrfosse99@gmail.com>
2016-11-06 16:26:04 -0800

    possible fix?

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	while (count < argc)
 	{
-		if (!atoi(argv[count]))
+		if (atoi(argv[count]) < 0 || !atoi(argv[count]))
 		{
 			printf("Error\n");
 			return (1);
