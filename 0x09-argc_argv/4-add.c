039d12805d1e70c4ba1b477e5646860ec0f1dab8
mrfosse <mrfosse99@gmail.com>
2016-11-06 16:17:00 -0800

    fixed always returning error.

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of arugments
+ * @argv: value of argument.
+ * Return: returns the total or error.
+ */
+int main(int argc, char *argv[])
+{
+	int temp, count;
+
+	temp = 0;
+	count = 1;
+
+	while (count < argc)
+	{
+		if (!atoi(argv[count]))
+		{
+			printf("Error");
+			return (1);
+		}
+		temp = temp + atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n", temp);
+	return (0);
+}
