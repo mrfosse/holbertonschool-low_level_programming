465aa7454f5bb103755a957d8d038d0ca72de1a0
mrfosse <mrfosse99@gmail.com>
2016-11-08 15:36:02 -0800

    fixes

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -2,26 +2,54 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- * create_array - creates an array of chars.
- * Description: and that initializes it with a specific char.
- * @str: string to be coppied..
+ * _strlen - measures the length of a string.
+ * Descritpion: measures the string s.
+ * @s: string to be messured/
+ * Return: returns length of s.
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+
+}
+
+/**
+ * str_concat - concatinates two string
+ * Description: and returns the new pointer.
+ * @s1: string one
+ * @s2: string two
  * Return: returns the pointer or NULL if it fails.
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	unsigned int count;
+	int count, count2;
 	char *a;
 
-	a = malloc(sizeof(str));
+	a = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (a == NULL)
 	{
 		return (NULL);
 	}
 	count = 0;
-	while (count <= sizeof(str))
+	while (count <= _strlen(s1))
 	{
-		a[count] = str[count];
+		a[count] = s1[count];
 		count++;
+	}
+	count--;
+	count2 = 0;
+	while (count2 <= _strlen(s2))
+	{
+		a[count] = s2[count2];
+		count++;
+		count2++;
 	}
 	a[count] = '\0';
 	return (a);
