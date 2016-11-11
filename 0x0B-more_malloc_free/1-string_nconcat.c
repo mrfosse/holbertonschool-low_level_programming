c04004034a797eea809636a937876c9cd9ded484
mrfosse <mrfosse99@gmail.com>
2016-11-11 10:53:29 -0800

    fixed 1 off issue.

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		k = count + n;
 
-	s = malloc((k + 1) * sizeof(char));
+	s = malloc((k + 1) * sizeof(*s));
 	count = 0;
 	if (s == NULL)
 		return (NULL);
