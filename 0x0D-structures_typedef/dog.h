6104bbf4e8f2d7e2fe8143f197d1d0b46621850c
mrfosse <mrfosse99@gmail.com>
2016-11-14 16:20:56 -0800

    add 0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG2
+#define DOG2
+/**
+ * dog - my first stuct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: who owns the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
