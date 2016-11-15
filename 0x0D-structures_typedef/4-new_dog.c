d9ac7ee987c086ba1befdf1a45fba710ecb854e4
mrfosse <mrfosse99@gmail.com>
2016-11-14 17:29:58 -0800

    betty doc fixes, still wont pass betty style

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,26 +1,27 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * new-dog - makes a new inits the dog structure
+ * new_dog - makes a new inits the dog structure
  *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  * Return: dog_t else NULL.
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *temp;
-	
+
 	temp = malloc(sizeof(dog_t));
 
-	if (temp == NULL)
-		return (NULL);
-	else
+	if (temp != NULL)
 	{
 		temp->name = name;
 		temp->age = age;
 		temp->owner = owner;
 		return (temp);
 	}
+	else
+		return (NULL);
 }
