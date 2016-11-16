76925992997ca70992489cbdd280c370d71f8166
mrfosse <mrfosse99@gmail.com>
2016-11-16 14:39:00 -0800

    possible fix

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,11 +1,11 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *  * _strlen - measures the length of a string.
- *   * Descritpion: measures the string s.
- *    * @s: string to be messured/
- *     * Return: returns length of s.
- *      */
+ * _strlen - measures the length of a string.
+ * Descritpion: measures the string s.
+ * @s: string to be messured/
+ * Return: returns length of s.
+ */
 int _strlen(char *s)
 {
 	int count;
@@ -31,6 +31,7 @@ int _strlen(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *temp;
+	int count;
 
 	temp = malloc(sizeof(dog_t));
 
@@ -38,21 +39,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	temp->name = name;
-	if (_strlen(temp->name) == 0)
+	temp->name = malloc(_strlen(name) * sizeof(temp->name));
+	if (temp == NULL)
 	{
-		free(temp->name);
-		free(temp);
 		return (NULL);
 	}
+	for (count = 0; count < _strlen(name); count++)
+		temp->name[count] = name[count];
+
 	temp->age = age;
-	temp->owner = owner;
-	if (_strlen(temp->owner) == 0)
+
+	temp->owner = malloc(_strlen(owner) * sizeof(temp->owner));
+	if (temp == NULL)
 	{
-		free(temp->name);
-		free(temp->owner);
-		free(temp);
 		return (NULL);
 	}
+	for (count = 0; count < _strlen(owner); count++)
+		temp->owner[count] = owner[count];
 	return (temp);
 }
