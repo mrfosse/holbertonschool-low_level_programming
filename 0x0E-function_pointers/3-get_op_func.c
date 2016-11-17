786ec346e2a1835b47ed72e81a6efe63837fb25b
mrfosse <mrfosse99@gmail.com>
2016-11-17 13:09:59 -0800

    updated betty and made changes to pass checks.

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -2,19 +2,19 @@
 #include "3-calc.h"
 /**
  * get_op_func - selects the proper function
- *
+ * Description: see above
  * @s: operator passed as an argument.
  * Return: returns pointer to the proper function based on operator given.
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
 
 	int i;
--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 /**
  * main - makes sure arguments are correct and calls a func
+ * Description: see above.
  * @argc: number of arguments
  * @argv: array of arguments
- * return: returns end result of arguments or 98-100 if error.
+ * Return: returns end result of arguments or 98-100 if error.
  */
 int main(int argc, char *argv[])
 {
@@ -19,17 +20,17 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return(98);
+		return (98);
 	}
 	if (p == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		return(99);
+		return (99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		return(100);
+		return (100);
 	}
 	printf("%d\n", p(num1, num1));
 	return (0);
