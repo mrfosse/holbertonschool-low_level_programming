83145757e8c590ef49777be3be4a813d2330b66b
mrfosse <mrfosse99@gmail.com>
2016-11-29 19:27:06 -0800

    betty fix

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
  *
  * Return: returns none.
  */
-void prefix (void) __attribute__ ((constructor));
+void prefix(void) __attribute__ ((constructor));
 
-void prefix (void)
+void prefix(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
