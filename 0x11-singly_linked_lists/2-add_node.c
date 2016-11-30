3e73194b42ee811a5f48fb465a00d13ead230191
mrfosse <mrfosse99@gmail.com>
2016-11-29 16:13:13 -0800

    removed redundent *p and used strdup instead.

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "lists.h"
 /**
  * add_node - prints all elements of the linked list.
@@ -10,26 +11,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
-	char *p;
 	int count;
 
 	count = 0;
-	p = malloc(sizeof(str) + 1);
-	if (p == NULL)
-		return (NULL);
 	while (str[count] != '\0')
 	{
-		p[count] = str[count];
 		count++;
 	}
-	p[count] = '\0';
 
 
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
 
-	temp->str = p;
+	temp->str = strdup(str);
 	temp->len = count;
 	temp->next = *head;
 	*head = temp;
