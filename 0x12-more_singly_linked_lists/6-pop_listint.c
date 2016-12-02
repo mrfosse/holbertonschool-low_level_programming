b2033ee9e6c96ab9c0137dc633dc042025859c1b
mrfosse <mrfosse99@gmail.com>
2016-12-01 17:31:35 -0800

    fix maybe?

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * pop-listint - delets the first item and returns the value.
- *
+ * pop-listint - delets the first item.
+ * Description: delets item than returns the value of deleted item.
  * @head: pointer to the start of the list.
  * Return: returns the nodes value n.
  */
