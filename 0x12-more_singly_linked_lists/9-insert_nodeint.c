8069bc158c79c2beff0dad77efca39148e89cd5e
mrfosse <mrfosse99@gmail.com>
2016-12-01 19:49:19 -0800

    betty fix

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	while (temp != NULL && count < index -1)
+	while (temp != NULL && count < index - 1)
 	{
 		temp = temp->next;
 		count++;
