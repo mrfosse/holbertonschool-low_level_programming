68d0a7f39024bd99ec90969f59f0f3dfdd15aa7b
mrfosse <mrfosse99@gmail.com>
2016-12-01 18:02:49 -0800

    changed var name

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -10,16 +10,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int N;
+	int num;
 
 	if (*head == NULL)
 		return (0);
 
 	temp = *head;
-	N = temp->n;
+	num = temp->n;
 
 	*head = temp->next;
 	free(temp);
 
-	return (N);
+	return (num);
 }
