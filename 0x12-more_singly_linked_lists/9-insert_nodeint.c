4275c9a9eb800e87138a1e295dd26315e037d92a
mrfosse <mrfosse99@gmail.com>
2016-12-01 20:01:46 -0800

    maybe?

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n;
 	if (index == 0)
 	{
 		new_node->next = *head;
@@ -40,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		return (NULL);
 	}
 
-	new_node->n = n;
+	
 	new_node->next = temp->next;
 	temp->next = new_node;
 
