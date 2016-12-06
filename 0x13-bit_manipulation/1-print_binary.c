76a51857d9c8112ff94e77b4e3b06f2f2c183edc
mrfosse <mrfosse99@gmail.com>
2016-12-05 17:03:11 -0800

    first working program, with proper output.

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_binary - prints binary form on int n.
+ *
+ * @n: number to printed to binary.
+ * Return: returns none.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask, flag;
+
+	flag = 0;
+	mask = 32768;
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+			;
+		else
+			flag = 1;
+
+		if (flag == 1)
+		{
+			if ((n & mask) == 0)
+				printf("0");
+			else
+				printf("1");
+		}
+
+		mask = mask >> 1;
+	}
+	if (flag == 0)
+		printf("0");
+}
+
