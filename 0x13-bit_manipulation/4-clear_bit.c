d173c0ac0c32c688cabf7aff15563947844ae5a0
mrfosse <mrfosse99@gmail.com>
2016-12-05 22:03:07 -0800

    first attempt.

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * clear_bit - toggles the bit to 0 at given index.
+ *
+ * @n: number to check
+ * @index: postion of bit to toggle
+ * Return: returns 1, else -1 on fail.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	
+
+	*n &= ~(1 << index);
+	
+	return (1);
+
+}
+
