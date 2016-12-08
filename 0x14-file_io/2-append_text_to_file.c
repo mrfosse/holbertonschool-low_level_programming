739703d24ab9fbb8942e73fa2e44256f4d51c6dd
mrfosse <mrfosse99@gmail.com>
2016-12-07 23:13:25 -0800

    fix to 1? added 2.

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -1,29 +1,23 @@
 #include "holberton.h"
 
 /**
- * create_file - reads a text file and prints to POSIX stdout.
+ * append_text_to_file - writes text to end of file..
  *
  * @filename: file to be written to.
  * @text_content: text to be written to file.
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, temp, count;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-	{
-		fd = fopen(filename, "a+");
-		close(fd);
-		return (1);
-	}
 
-	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
+	
 	count = 0;
 	while (text_content[count] != '\0')
 	{
