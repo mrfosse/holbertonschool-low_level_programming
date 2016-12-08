17b6e13252a3b979ab24eb8375e09718129170d0
mrfosse <mrfosse99@gmail.com>
2016-12-07 23:15:48 -0800

    betty fix 2, commented last fix for 1 out, broke everything.... =(

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,13 +13,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-	{
-		fd = fopen(filename, "a+");
-		close(fd);
-		return (1);
-	}
-
+/**	if (text_content == NULL)
+*	{
+*		fd = fopen(filename, "a+");
+*		close(fd);
+*		return (1);
+*	}
+*/
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	
+
 	count = 0;
 	while (text_content[count] != '\0')
 	{
