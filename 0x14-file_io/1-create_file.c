d5da6c16fb682ada24657d06802f4c5d69f5078b
mrfosse <mrfosse99@gmail.com>
2016-12-07 20:03:40 -0800

    took out extra var, changed counter.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,27 +10,33 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, temp, count;
-	const char *tempf;
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	count = 0;
-	tempf = filename;
-	while (*tempf != '\0')
+	printf("%d\n", count);
+	while (text_content[count] != '\0')
 	{
-		tempf++;
 		count++;
+		printf("%d\n", count);
 	}
-
+	printf("%d, last before print\n", count);
 	temp = write(fd, text_content, count);
-	close(fd);
+	printf("%c\n", temp);
+	
 	if (temp == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	else
+	{
+		close(fd);
 		return (1);
+	}
 }
