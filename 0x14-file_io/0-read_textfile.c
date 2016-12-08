d9581e7985b2d29bb11c90c1987d45959afe1823
mrfosse <mrfosse99@gmail.com>
2016-12-07 16:09:43 -0800

    first attemped at 1, skipped 0 for now.

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints to POSIX stdout.
+ *
+ * @filename: file to be opened and read.
+ * @letters: number of letters it should read and print.
+ * Return: returns the actual number of letters it could read.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - reads a text file and prints to POSIX stdout.
+ *
+ * @filename: file to be written to.
+ * @text_content: text to be written to file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, temp, count;
+	const char *tempf;
+
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	if (fd == -1)
+		return (0);
+	
+	count = 0;
+	tempf = filename;
+	while (*tempf != '\0')
+	{
+		tempf++;
+		count++;
+	}
+	
+	temp = write(fd, text_content, count);
+	close(fd);
+	if (temp == -1)
+		return (-1);
+	else
+		return (1);
+}
