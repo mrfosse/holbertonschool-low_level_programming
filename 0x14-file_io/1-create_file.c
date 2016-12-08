4112597bd425126cd8db2e3801834c65aed36220
mrfosse <mrfosse99@gmail.com>
2016-12-07 16:16:20 -0800

    added check o see if filename is null.

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -12,6 +12,9 @@ int create_file(const char *filename, char *text_content)
 	int fd, temp, count;
 	const char *tempf;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1)
 		return (0);
