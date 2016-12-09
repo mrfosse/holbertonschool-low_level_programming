35298b29dee3c8fcae2c3ebb587ba5945675b619
mrfosse <mrfosse99@gmail.com>
2016-12-08 17:21:44 -0800

    added free to all returns

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -15,31 +15,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(sizeof(*buf) * letters);
 	if (buf == NULL)
-		return (0);
+		free(buf); return (0);
 
 	if (filename == NULL)
-		return (0);
+		free(buf); return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
+		free(buf); return (0);
 
 	count = read(fd, buf, letters);
 	if (count == -1)
-		return (0);
+		free(buf); return (0);
 
 	if (count > 0)
 		count = write(STDOUT_FILENO, buf, count);
 
 	if (count == -1)
-		return (0);
+		free(buf); return (0);
 
 	temp = close(fd);
 	if (temp == -1)
 	{
-		free(buf);
-		return (0);
+		free(buf); return (0);
 	}
-	free(buf);
-	return (count);
+	free(buf); return (count);
 }
