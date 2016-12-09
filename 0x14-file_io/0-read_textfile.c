c799684f2f1f950ade0d9048fa40ed76ef3c8fcb
mrfosse <mrfosse99@gmail.com>
2016-12-08 17:52:31 -0800

    fix?

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -9,35 +9,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t temp, count;
+	int fd, temp, count, writeval;
 	char *buf;
 
 	buf = malloc(sizeof(*buf) * letters);
 	if (buf == NULL)
-		free(buf); return (0);
+		return (0);
 
 	if (filename == NULL)
-		free(buf); return (0);
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		free(buf); return (0);
+		return (0);
 
 	count = read(fd, buf, letters);
 	if (count == -1)
-		free(buf); return (0);
+		return (0);
 
 	if (count > 0)
-		count = write(STDOUT_FILENO, buf, count);
+		writeval = write(STDOUT_FILENO, buf, count);
 
-	if (count == -1)
-		free(buf); return (0);
+	printf("%d" , writeval);
+	if (writeval == -1 || writeval != count)
+		return (0);
 
 	temp = close(fd);
 	if (temp == -1)
-	{
-		free(buf); return (0);
-	}
-	free(buf); return (count);
+		return (0);
+
+	free(buf); 
+	return (writeval);
 }
