21c7d428c3169bce52ecb650f952680cd73eefc4
mrfosse <mrfosse99@gmail.com>
2017-03-07 21:12:56 -0800

    added edge case.

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -28,6 +28,11 @@ char *str_concat(char *s1, char *s2)
 	char *temp;
 	int i, h;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	temp = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (temp == NULL)
 		return (NULL);
