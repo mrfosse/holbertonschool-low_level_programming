e370a56b7e399581595c38d921b490aefae3c31a
mrfosse <mrfosse99@gmail.com>
2017-03-07 20:32:49 -0800

    adding 3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * *alloc_grid - makes a grid.
+ * @width: width of the grid.
+ * @height: height of the grid.
+ * Return: Returns the pointer, NULL on fail.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **temp;
+	int i, h;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	temp = malloc(width * sizeof(int *));
+	if (temp == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+		temp[i] = malloc(width * sizeof(int));
+	for (i = 0; i < height; i++)
+		for (h = 0; h < width; h++)
+			temp[i][h] = 0;
+
+	if (temp)
+		return (temp);
+	else
+		return (NULL);
+}
