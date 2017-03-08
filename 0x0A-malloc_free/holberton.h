ebad5f6c786dc8eb4537c3d3fc9b25943a53bbfc
mrfosse <mrfosse99@gmail.com>
2017-03-07 16:52:04 -0800

    adding 0.

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -1,6 +1,10 @@
-#ifndef HOLBERTON.H
-#define HOLBERTON.h
+#ifndef HOLBERTON
+#define HOLBERTON
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
+char *create_array(unsigned int size, char c);
 
 #endif
