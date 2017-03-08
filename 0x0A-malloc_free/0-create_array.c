b111f32dad4531bfc1cea01868287112184b9c7e
mrfosse <mrfosse99@gmail.com>
2017-03-07 17:51:03 -0800

    testing for last check

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,21 +1,29 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * create_array - creats an array with a specific char.
+ * create_array - creates an array of chars.
+ * Description: and that initializes it with a specific char.
  * @size: size of the array.
- * @c: the char to add
- * Return: Returns the pointer, NULL on fail.
+ * @c: the array.
+ * Return: returns the pointer or NULL if it fails.
  */
 char *create_array(unsigned int size, char c)
 {
-	char *temp;
-	unsigned int i;
+	unsigned int count;
+	char *a;
 
-	temp = malloc((size + 1) * sizeof(char));
-	if (temp == NULL)
+	a = malloc((size + 1) * sizeof(char));
+	if (a == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < size; i++)
-		temp[i] = c;
-	temp[i] = '\0';
-	return (temp);
+	}
+	count = 0;
+	while (count < size)
+	{
+		a[count] = c;
+		count++;
+	}
+	a[count] = '\0';
+	return (a);
 }
---
+++ 0x0A-malloc_free/0-create_array2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * create_array - creats an array with a specific char.
+ * @size: size of the array.
+ * @c: the char to add
+ * Return: Returns the pointer, NULL on fail.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *temp;
+	unsigned int i;
+
+	temp = malloc((size + 1) * sizeof(char));
+	if (temp == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		temp[i] = c;
+
+	i++;
+	temp[i] = '\0';
+	return (temp);
+}
