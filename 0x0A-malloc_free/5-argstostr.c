16d656158994e70aad8d50de2c134141a0861883
mrfosse <mrfosse99@gmail.com>
2017-03-07 21:02:10 -0800

    fix for 0

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+
+
+/**
+ * _strdup - duplcites a string.
+ * @str: the string to be coppied.
+ * Return: Returns the pointer, NULL on fail.
+ */
+char *_strdup(char *str)
+{
+
+}
+/**
+ * argstostr - measures the length of a string.
+ * Descritpion: measures the string s.
+ * @s: string to be messured/
+ * Return: returns length of s.
+ */
+char *argstostr(int ac, char **av)
+{
+
+}
