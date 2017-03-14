250e083ec31b6c7e5ade7917a0071ad20a2cb88c
mrfosse <mrfosse99@gmail.com>
2017-03-13 23:16:57 -0700

    adding 5

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - free momory from dog
+ *
+ * @d: pointer to free
+ * Return: none.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+	else
+		return;
+}
+
