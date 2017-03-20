744ed416131f2296de259e208748052b3cc14f7c
mrfosse <mrfosse99@gmail.com>
2017-03-19 22:05:46 -0700

    adding 2.

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints all parameters.
+ * @separator: seperates  arguments.
+ * @n: number of args.
+ * Return: none.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list add;
+	char *p;
+
+	va_start(add, n);
+
+	for (i = 0; i < n; i++)
+	{
+		p = va_arg(add, char *);
+		if (p == NULL)
+			printf("(nill");
+		else
+			printf("%s", p);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(add);
+}
