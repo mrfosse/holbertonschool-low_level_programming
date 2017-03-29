073c55805ccaf732b020b57d276d02d80bcbe39b
mrfosse <mrfosse99@gmail.com>
2017-03-28 22:05:51 -0700

    adding 7.

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -8,19 +8,12 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-/*	listint_t *s; */
 
-/*	s* = malloc(sizeof(listint_t));
-	if (s == NULL)
-		return (NULL); */
-
-/*	s = head; */
-	for (i = 0; i < index && head != NULL; i++)
+	for (i = 0; i < index; i++)
 	{
+		if (head == NULL)
+			return(0);
 		head = head->next;
 	}
-	if (i != index)
-		return (NULL);
-
-	return (&head->n);
+	return (head);
 }
