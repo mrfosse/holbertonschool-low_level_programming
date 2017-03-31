615213615a89530d872e00c0e685628df892e6a3
mrfosse <mrfosse99@gmail.com>
2017-03-30 19:46:58 -0700

    adding 0 and lib.

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * binary_to_unit - binary to unsigned int.
+ * @b: string.
+ * Return: returns converted number, or 0 if fail.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for ( i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0'  && b[i] != '1')
+			return (0);
+	}
+	for ( i = 0, num = 0; b[i] != '\0'; i++)
+		num = (num << 1) | (b[i] - 48);
+	return (num);
+}
